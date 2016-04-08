13be64de53f340049505ea5fbdb28df340ad0991
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-08 16:41:01 -0400

    script for all privileged setup steps

---
+++ script/privileged_setup.sh
@@ -0,0 +1,8 @@
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install git build-essential cmake ruby-build silversearcher-ag postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3 libyaml-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev libpq-dev nodejs-legacy npm
+sudo -u postgres createuser --superuser vagrant
