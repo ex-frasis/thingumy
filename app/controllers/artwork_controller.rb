fb0de498d335c68f0eafe03c9f7974f371e60cae
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-28 23:11:26 -0400

    basic views for the artwork

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -1,2 +1,10 @@
 class ArtworkController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'static_page#home'
 
   get '/home' => 'static_page#home'
+
+  # artwork
+  get 'artwork/index' => 'artwork#index'
+  get 'artwork/:id' => 'artwork#show'
 end
