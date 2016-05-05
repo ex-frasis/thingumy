9e1381209fc160aacd0ef4be292d762f939160d2
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-05 16:11:56 -0400

    about page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -3,4 +3,8 @@ class StaticPageController < ApplicationController
   def home
     render :home
   end
+
+  def about
+    render :about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_page#home'
 
   get '/home' => 'static_page#home'
+  get '/about' => 'static_page#about'
 
   # artwork
   get 'artwork/' => 'artwork#index'
