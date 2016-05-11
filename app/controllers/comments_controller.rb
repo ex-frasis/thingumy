c9f919aa156885b5bc7af38ea97ade3b22ee8f96
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-11 15:40:09 -0400

    comments! they work!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    binding.remote_pry
-    puts params
+    @artwork = Artwork.find_by_id(artwork_id)
+    @comment = Comment.create(
+      artwork: @artwork,
+      text: comment_text
+    )
+    redirect_to "#{artwork_path}/#{@artwork.id}"
+  end
+
+  protected
+
+  def comment_text
+    params.require(:comment)["text"]
+  end
+
+  def artwork_id
+    params.require(:artwork_id)
   end
 end
