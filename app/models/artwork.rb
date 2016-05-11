fd96796e4818c4a6c46925aac9a29c895d8ddee4
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-11 13:04:22 -0400

    comments are a thing kinda

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,6 +2,7 @@ class Artwork < ActiveRecord::Base
   has_and_belongs_to_many :artists
   has_and_belongs_to_many :subjects
   has_many :images
+  has_many :comments
 
   def artist_name
     self.artists.first.to_s
