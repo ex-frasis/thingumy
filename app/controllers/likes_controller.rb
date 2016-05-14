94558368ffa32bcff5b349698e1765a8b38cc781
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-14 10:20:59 -0400

    likes!

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,6 @@
+class LikesController < ApplicationController
+
+  def create
+    binding.pry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get '/home' => 'static_page#home'
   get '/about' => 'static_page#about'
 
-  # artwork
   get 'artwork/' => 'artwork#index'
   get 'artwork/:id' => 'artwork#show'
 
   resources :comments
+  resources :likes
 end
