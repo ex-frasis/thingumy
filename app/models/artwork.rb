79b4a379c8af2d2eb898356b0ab20bcdf4faa9fc
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-14 10:17:47 -0400

    add some relation statements

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,6 +3,7 @@ class Artwork < ActiveRecord::Base
   has_and_belongs_to_many :subjects
   has_many :images
   has_many :comments
+  has_many :likes
 
   def artist_name
     self.artists.first.to_s
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ActiveRecord::Base
+  has_one :artwork
+  has_one :user
 end
