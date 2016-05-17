11969f4efc76ffbda66dda021f9aea09c5e63bb7
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-16 23:39:59 -0400

    fix issues with jQuery ready and turbolinks

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,6 +1,8 @@
-$(document).ready(function () {
-  $(".artist-list").css(
-    "height",
+var artistListHeight = function () {
+  $(".artist-list").css("height",
     window.screen.height - 180
   );
-});
+}
+
+$(document).ready(artistListHeight);
+$(document).on('page:load', artistListHeight);
--- app/assets/javascripts/static_page.js
+++ app/assets/javascripts/static_page.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
+var aboutPageHeight = function () {
   $("#about-page").css(
     "height",
     window.screen.height - 105
   );
-});
+}
+
+$(document).ready(aboutPageHeight);
+$(document).on('page:load', aboutPageHeight);
