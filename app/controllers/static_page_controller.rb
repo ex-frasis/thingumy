fd621b96690733e169bf0ea1f85c536260e15d9b
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-19 23:45:23 -0400

    methods in static pages for the home page

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,11 +1,20 @@
 class StaticPageController < ApplicationController
-
   def sign_in
     render :sign_in
   end
 
   def home
+    recent_comments
+    top_artists
     render :home
+  end
+
+  def recent_comments
+    @recent_comments = Comment.order(created_at: :desc).limit(10)
+  end
+
+  def top_artists
+    @top_artists = Like.group(:artwork_id).count.entries.sort{|x,y| y[1] <=> x[1]}[0..5].map{|id| Artwork.find_by_id(id[1])}.uniq
   end
 
   def about
