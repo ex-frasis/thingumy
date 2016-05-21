734ae2ac3d0ed501710dfd8909ab4a9ed3583e2a
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-21 13:12:11 -0400

    add a button that shows / hides artist statement

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -1,13 +1,12 @@
-var sliderize = function () {
-  $('#artwork-slider-left').slideReveal({
-    trigger: $('#trigger-left')
-  });
-
-  $('#artwork-slider-right').slideReveal({
-    trigger: $('#trigger-right'),
-    position: "right"
-  });
+function toggleStatementShow () {
+  var statementText = $("#artist-statement-text");
+  statementText.css("display", statementText.css("display") === "none" ? "inherit" : "none");
 }
 
-$(document).ready(sliderize);
-$(document).on('page:load', sliderize);
+function setStatementOnClick () {
+  console.log("doing it");
+  $("#expand-artist-statement").click(toggleStatementShow);
+}
+
+$(document).ready(setStatementOnClick);
+$(document).on('page:load', setStatementOnClick);
